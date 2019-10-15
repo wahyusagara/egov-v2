c00cb8d636c6d766ab386a6e288100eacea03465
Syahrulloh <syahrulloh3@gmail.com>
2019-10-16 01:05:17 +0700

    canlendar, npm install required

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { CalendarModule } from 'ion2-calendar';
 
 const routes: Routes = [
   {
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    CalendarModule
   ],
   declarations: [HomePage]
 })
