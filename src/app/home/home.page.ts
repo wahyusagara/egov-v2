b59c34de389616dea16bb31da6a3a58172ed44c1
DESKTOP-LKJ5344\M Bagus Santoso <muhamadbagussantoso@gmail.com>
2019-10-15 14:23:13 +0700

    Heroku Dpeloyment.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { NavController, AlertController, Platform } from 'ionic-angular';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+ 
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private platform: Platform) {
+ 
+  }
+ 
+  showPlatform() {
+    let text = 'I run on: ' + this.platform.platforms();
+    let alert = this.alertCtrl.create({
+      title: 'My Home',
+      subTitle: text,
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
+ 
 }
