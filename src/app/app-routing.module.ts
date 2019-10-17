b781bea82d09a2f5c1df3ef3f4e7c93bd8b9026c
Syahrulloh <syahrulloh3@gmail.com>
2019-10-17 20:55:42 +0700

    testing rebuild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,6 @@ const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full'
   },
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  // },
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
