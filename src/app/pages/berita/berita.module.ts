f1fe6f830ac9336b9d0dc4cf1450d8d4c292e7b5
muhamadbagussantoso <muhamadbagussantoso@gmail.com>
2019-11-21 03:23:40 +0700

    Fix - centent-text slider home - Berita - Profile

--- src/app/pages/berita/berita.module.ts
+++ src/app/pages/berita/berita.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { BeritaPage } from './berita.page';
+import { ComponentModule } from "../../component/component.module";
 
 const routes: Routes = [
   {
@@ -19,8 +20,10 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [BeritaPage]
+  declarations: [BeritaPage],
+  schemas:[ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class BeritaPageModule {}
