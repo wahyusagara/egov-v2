0a113a838c4ad8ef1549dda65b8952de01732bf9
DESKTOP-LKJ5344\M Bagus Santoso <muhamadbagussantoso@gmail.com>
2019-12-09 19:23:36 +0700

    init page form perjalanan dinas

--- src/app/pages/maintenance/maintenance.page.ts
+++ src/app/pages/maintenance/maintenance.page.ts
@@ -5,11 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './maintenance.page.html',
   styleUrls: ['./maintenance.page.scss'],
 })
-export class MaintenancePage implements OnInit {
 
+export class MaintenancePage implements OnInit {
+  text:any = {
+    Year: 'Year',
+    Month: 'Month',
+    Weeks: "Weeks",
+    Days: "Days",
+    Hours: "Hours",
+    Minutes: "Minutes",
+    Seconds: "Seconds",
+    MilliSeconds: "MilliSeconds"
+  };
+  
   constructor() { }
 
   ngOnInit() {
   }
-
+  onEvent($event): void {
+    let timeLeft = $event.left;
+  }
 }
