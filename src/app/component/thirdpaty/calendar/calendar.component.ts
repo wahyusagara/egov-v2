8c802c267f97eb3dfee600136d301bd55bb8550a
Syahrulloh <syahrulloh3@gmail.com>
2019-12-20 05:31:30 +0700

    calendar data api, and ui

--- src/app/component/thirdpaty/calendar/calendar.component.ts
+++ src/app/component/thirdpaty/calendar/calendar.component.ts
@@ -19,6 +19,7 @@ import {
 } from 'angular-calendar';
 import { GlobalFuncService } from 'src/app/services/global-func.service';
 import { Subject } from 'rxjs';
+import { ApiService } from 'src/app/services/api/api.service';
 
 const colors: any = {
   red: {
@@ -81,50 +82,66 @@ export class CalendarComponent implements OnInit {
   ];
 
   events: CalendarEvent[] = [
-    {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'A 3 day event',
-      color: colors.red,
-      actions: this.actions,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true
-      },
-      draggable: true
-    },
-    {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date',
-      color: colors.yellow,
-      actions: this.actions
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
-      color: colors.blue,
-      allDay: true
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: addHours(new Date(), 2),
-      title: 'A draggable and resizable event',
-      color: colors.yellow,
-      actions: this.actions,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true
-      },
-      draggable: true
-    }
+    // {
+    //   start: subDays(startOfDay(new Date()), 1),
+    //   end: addDays(new Date(), 1),
+    //   title: 'A 3 day event',
+    //   color: colors.red,
+    //   actions: this.actions,
+    //   allDay: true,
+    //   resizable: {
+    //     beforeStart: true,
+    //     afterEnd: true
+    //   },
+    //   draggable: true
+    // },
+    // {
+    //   start: startOfDay(new Date()),
+    //   title: 'An event with no end date',
+    //   color: colors.yellow,
+    //   actions: this.actions
+    // },
+    // {
+    //   start: subDays(endOfMonth(new Date()), 3),
+    //   end: addDays(endOfMonth(new Date()), 3),
+    //   title: 'A long event that spans 2 months',
+    //   color: colors.blue,
+    //   allDay: true
+    // },
+    // {
+    //   start: addHours(startOfDay(new Date()), 2),
+    //   end: addHours(new Date(), 2),
+    //   title: 'A draggable and resizable event',
+    //   color: colors.yellow,
+    //   actions: this.actions,
+    //   resizable: {
+    //     beforeStart: true,
+    //     afterEnd: true
+    //   },
+    //   draggable: true
+    // }
   ];
   refresh: Subject<any> = new Subject();
 
+  a = [
+    {
+      a: "1"
+    },
+    {
+      a: "2"
+    },
+    {
+      a: "3"
+    }
+  ];
   constructor(
-    private global: GlobalFuncService
-  ) {}
+    private global: GlobalFuncService,
+    private api: ApiService
+  ) {
+    
+    console.log(this.a);
+    console.log(this.a.findIndex(x => x.a == '1'));
+  }
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     this.global.showToast('Tanggal ' + date + ' Clicked', 'primary')
@@ -163,6 +180,50 @@ export class CalendarComponent implements OnInit {
     this.handleEvent('Dropped or resized', event);
   }
 
-  ngOnInit() {}
+  getDataCalendars() {
+    return this.api.getData('https://sdm.big.go.id/siap/service/index.php/pegawai/logbook').then((result) => {
+      console.log(result);
+      const resp = JSON.parse(JSON.stringify(result));
+      const start = new Date(resp[0].TANGGAL.substring(0,8));
+      const end = addDays(new Date(resp[0].TANGGAL.substring(0,8)), parseInt(resp[0].LAMA_PENGERJAAN));
+      console.log(start, end);
+      // this.events = [];
+      var eventCalendars: CalendarEvent[] = [];
+      resp.forEach((val,idx) => {
+        let warna;
+        if (val.KATEGORI === '1') {
+          warna = colors.yellow;
+        } else if(val.KATEGORI === '2') {
+          warna = colors.red;
+        } else {
+          warna = colors.blue;
+        };
+        if (eventCalendars.findIndex(x => x.id == val.KODESKP) == -1) {
+          eventCalendars.push({
+            start: new Date(val.TANGGAL.substring(0,9)),
+            // end: addDays(new Date(val.TANGGAL.substring(0,9)), parseInt(val.LAMA_PENGERJAAN)),
+            title: val.URAIAN_AKTIVITAS ? val.URAIAN_AKTIVITAS : "",
+            allDay: true,
+            draggable: false,
+            resizable: {
+              afterEnd: false,
+              beforeStart: false
+            },
+            id: val.KODESKP,
+            actions: this.actions,
+            color: warna
+          })
+        }
+      });
+      return this.events = eventCalendars;
+      console.log(this.events);
+    }).catch((err) => {
+      console.error(err);
+    })
+  }
+
+  async ngOnInit() {
+    await this.getDataCalendars();
+  }
 
 }
